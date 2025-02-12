cd5e5331069d24b0f1a081b002b3dbd329276e48
Zakarum <zaq.dev@icloud.com>
2025-02-12 21:34:34 +0100

    wip

--- website/src/markdown.rs
+++ website/src/markdown.rs
@@ -1,10 +1,16 @@
-
 use dioxus::prelude::*;
 use pulldown_cmark::Parser;
 
 /// Render some text as markdown.
 #[component]
-pub fn Markdown(content: String) -> Element {
+pub fn Markdown(url: String) -> Element {
+    let content = use_resource(move || {
+        let url = url.clone();
+        async move { reqwest::get(url).await.unwrap().text().await.unwrap() }
+    });
+
+    let content = content.cloned().unwrap_or_default();
+
     let parser = Parser::new(&content);
 
     let mut html_buf = String::new();
